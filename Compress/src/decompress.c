78e53c4bdb9835dc14919b1aae45c5c02048338e
Rosana Santos <30476797+rosanass@users.noreply.github.com>
2018-10-09 14:17:41 -0300

    Preorder fixed

--- Compress/src/decompress.c
+++ Compress/src/decompress.c
@@ -11,7 +11,7 @@ void start_decompression(FILE* archive, FILE* new_archive)
     int tree_size = get_header_tree_size(archive);
     unsigned char* pre_order = get_header_pre_order(archive, tree_size);
     ht* huff_tree = create_empty_ht();
-    int pos=0;
+    int pos = 0;
     set_tree_node(huff_tree, remake_huff_tree(pre_order, &pos, tree_size));
     printf("");
 }
@@ -70,28 +70,30 @@ unsigned char* get_header_pre_order(FILE* archive, int tree_size)
 }
 
 
-node* remake_huff_tree(unsigned char *pre_order, int* pos, int tree_size)
+node* remake_huff_tree(unsigned char *pre_order, int * pos, int tree_size)
 {
     if(*pos >= tree_size)
     {
         return NULL;
     }
-    unsigned char c = pre_order[*pos];
+    unsigned char * c =  (unsigned char *)malloc(sizeof(unsigned char));
+    *c= pre_order[*pos];
+    //unsigned char c = pre_order[*pos];
     node* new_node;
-    if(c  == '*')
+    if(*c  == '*')
     {
-        new_node = create_node('*', 0);
+        new_node = create_node(c, 0);
         *pos = *pos+1;
-        set_node_left(new_node, remake_huff_tree(pre_order, *pos, tree_size));
-        set_node_right(new_node, remake_huff_tree(pre_order, *pos, tree_size));
+        set_node_left(new_node, remake_huff_tree(pre_order, pos, tree_size));
+        set_node_right(new_node, remake_huff_tree(pre_order, pos, tree_size));
         return new_node;
     }
     else
     {
-        if (c == '\\')
+        if (*c == '\\')
         {
             *pos = *pos+1;
-            c = pre_order[*pos];
+            *c = pre_order[*pos];
             new_node = create_node(c, 0);
             *pos = *pos+1;
         }
