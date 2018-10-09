24a7f253570a9a5792909a24d743c4263510f6e2
Rosana Santos <30476797+rosanass@users.noreply.github.com>
2018-10-09 14:03:08 -0300

    Some changes

--- Compress/main.c
+++ Compress/main.c
@@ -5,7 +5,7 @@
 
 int main() {
     int type;
-    char source[]="D:\\x.jpg", dest[]="D:\\x.huff";
+    char source[]="D:\\jaehnatal.jpg", dest[]="D:\\x.huff";
     char source1[]="D:\\out1.txt", dest1[]="D:\\teste1d.txt";
 
     printf("Digite 1 para Compressão ou 2 para Descompressão:\n");
@@ -13,11 +13,6 @@ int main() {
     //getchar();
     if(type == 1)
     {
-        //printf("Onde está o arquivo de origem?\n");
-        //scanf("%s", source);
-        //printf("Onde o arquivo vai ser salvo?\n");
-        //scanf("%s", dest);
-
         FILE *archive = fopen(source, "rb");
         FILE *new_archive = fopen(dest, "wb");
         start_compression(archive, new_archive);
@@ -26,10 +21,7 @@ int main() {
     }
     else if(type == 2)
     {
-        //printf("Onde está o arquivo de origem?\n");
-        //scanf("%s", source);
         printf("Onde o arquivo vai ser salvo?\n");
-        //scanf("%s", dest);
         FILE *archive = fopen(source1, "rb");
         FILE *new_archive = fopen(dest1, "wb");
         start_decompression(archive, new_archive);
